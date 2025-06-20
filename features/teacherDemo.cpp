f10a92b7494ddff516da77bbe066e6ab6c1c20f6
kk <lyvanna544@gmail.com>
2025-06-20 08:56:02 +0700

    some changes

--- features/teacherDemo.cpp
+++ features/teacherDemo.cpp
@@ -1,6 +1,11 @@
 #include<iostream> 
 using namespace std; 
 
+class Teacher {
+    private: 
+        string uuid; 
+        string name; 
+}
 int main(){
     system("clear"); 
     cout<<"Teacher Demo Class "<<endl; 
