8a3abf3665ed338eb28f0d2756eee42d8f6f6599
sexymanalive <lyvanna544@gmail.com>
2025-06-20 01:50:52 +0000

    adding teacher class too

--- features/teacherDemo.cpp
+++ features/teacherDemo.cpp
@@ -1,6 +1,15 @@
 #include<iostream> 
 using namespace std; 
 
+class Teacher{
+    public: 
+        string uuid; 
+        string teacherName; 
+        Teacher(string uuid, string teacherName){
+            this-> uuid = uuid; 
+            this-> teacherName = teacherName; 
+        }
+}
 int main(){
     system("clear"); 
     cout<<"Teacher Demo Class "<<endl; 
