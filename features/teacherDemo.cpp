ffc37cad4ac50dbe2d5b8a10d2da88e45edea480
sexymanalive <lyvanna544@gmail.com>
2025-06-20 01:54:06 +0000

    latest code on master

--- features/teacherDemo.cpp
+++ features/teacherDemo.cpp
@@ -1,16 +1,12 @@
 #include<iostream> 
 using namespace std; 
-class Teacher{
-    private: 
-        string name; 
-        int age; 
-}; 
 
 class Teacher{
-    public: 
+   private: 
         string uuid; 
         string teacherName; 
-        Teacher(string uuid, string teacherName){
+   public:
+         Teacher(string uuid, string teacherName){
             this-> uuid = uuid; 
             this-> teacherName = teacherName; 
         }
